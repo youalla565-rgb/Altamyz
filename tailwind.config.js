d2c8c44ad58210795841cfce82143b88bf5396e3
youalla565-rgb <youalla565@gmail.com>
2026-07-20 17:52:04 +0300

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+
+content:[
+"./index.html",
+"./src/**/*.{js,jsx}"
+],
+
+theme:{
+extend:{
+colors:{
+primary:"#2563EB",
+secondary:"#0F172A",
+gold:"#FDBA21",
+light:"#F8FAFC"
+}
+}
+},
+
+plugins:[]
+}
