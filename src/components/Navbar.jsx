fd27a6681ada3fabf2a246a7dde1077424ccfabe
youalla565-rgb <youalla565@gmail.com>
2026-07-20 19:17:44 +0300

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white p-4">
+      <h1 className="text-2xl font-bold">
+        منصة التميز التعليمي
+      </h1>
+    </nav>
+  );
+}
